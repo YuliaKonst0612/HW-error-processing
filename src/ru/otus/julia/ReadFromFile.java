741edfb1d4b5110ad3731495e457be700fad9286
Yulia KOnstantinova <yulia1988const@gmail.com>
2021-09-08 17:14:27 +0300

    -m

---
+++ src/ru/otus/julia/ReadFromFile.java
@@ -0,0 +1,34 @@
+package ru.otus.julia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadFromFile {
+    public static void main(String[] args) {
+
+        try {
+            readFile();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+
+    }
+
+    private static void readFile() throws FileNotFoundException {
+        File file = new File("h");
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
